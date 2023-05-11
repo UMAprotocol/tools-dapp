24224ab6a6e2c3aa70bdaebe104eae9f1736482d
ryanwolhuter <dev@ryanwolhuter.com>
2023-05-11 14:56:07 +0200

    apply prettier
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Tools for UMA',
-  description: 'Making life easier for UMAns',
-}
+  title: "Tools for UMA",
+  description: "Making life easier for UMAns",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
