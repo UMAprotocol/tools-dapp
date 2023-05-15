3efc4e1b60c5826bc74be4e986f7b91b669f939e
ryanwolhuter <dev@ryanwolhuter.com>
2023-05-15 11:07:53 +0200

    add test implementation of fonts
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import NextLink from "next/link";
-
+import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>Dashboard</h1>
+      <h1 className={styles.header}>Dashboard</h1>
+      <h1 className={styles.header2}>Test test</h1>
+      <h1 className={styles.header3}>Test test</h1>
       <NextLink href="/assertion-tool">Assertion Tool</NextLink>
     </main>
   );
