c333bbedba39c2faa37a081196f82deb03d2cd51
ryanwolhuter <dev@ryanwolhuter.com>
2023-05-17 13:08:02 +0200

    add tooltip component
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,28 @@
+import * as RadixTooltip from "@radix-ui/react-tooltip";
+import styles from "./Tooltip.module.css";
+interface Props {
+  children: React.ReactNode;
+  content: React.ReactNode;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+}
+export function Tooltip({ children, content, open, onOpenChange }: Props) {
+  return (
+    <RadixTooltip.Provider delayDuration={100}>
+      <RadixTooltip.Root open={open} onOpenChange={onOpenChange}>
+        <RadixTooltip.Trigger asChild>
+          <span className={styles.trigger}>{children}</span>
+        </RadixTooltip.Trigger>
+        <RadixTooltip.Portal>
+          <RadixTooltip.Content
+            sideOffset={4}
+            alignOffset={4}
+            className={styles.content}
+          >
+            {content}
+          </RadixTooltip.Content>
+        </RadixTooltip.Portal>
+      </RadixTooltip.Root>
+    </RadixTooltip.Provider>
+  );
+}
