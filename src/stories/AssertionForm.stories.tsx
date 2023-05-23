7dfd04eed8a7de46d49c9afa0d4a3fe1e0f7c093
ryanwolhuter <dev@ryanwolhuter.com>
2023-05-23 12:19:28 +0200

    add wallet config to story
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/stories/AssertionForm.stories.tsx
+++ src/stories/AssertionForm.stories.tsx
@@ -1,5 +1,6 @@
 import { AssertionForm } from "@/components";
 import type { Meta, StoryObj } from "@storybook/react";
+import { MockWalletConfig } from "./MockWalletConfig";
 
 const meta: Meta = {
   component: AssertionForm,
@@ -9,7 +10,15 @@ export default meta;
 
 type Story = StoryObj<typeof AssertionForm>;
 
-const Template: Story = {};
+const Template: Story = {
+  render: function Wrapper() {
+    return (
+      <MockWalletConfig>
+        <AssertionForm />
+      </MockWalletConfig>
+    );
+  },
+};
 
 export const Default = {
   ...Template,
