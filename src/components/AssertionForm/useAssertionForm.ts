82792b16e65101b72ca3b75a0cd61a0a3160c4c7
ryanwolhuter <dev@ryanwolhuter.com>
2023-05-31 15:20:48 +0200

    check if is connected in insufficient funds
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/AssertionForm/useAssertionForm.ts
+++ src/components/AssertionForm/useAssertionForm.ts
@@ -4,9 +4,10 @@ import {
   currenciesByChain,
   oov3AddressesByChainId,
 } from "@/constants";
-import { useMinimumBond } from "@/hooks";
-import type { ChainId, DropdownItem } from "@/types";
-import { useEffect, useState } from "react";
+import { truncateDecimalString } from "@/helpers";
+import { useBalanceAndAllowance, useMinimumBond } from "@/hooks";
+import type { ChainId, ChainName, DropdownItem } from "@/types";
+import { useEffect, useMemo, useState } from "react";
 import { useUpdateEffect } from "usehooks-ts";
 import type { Address } from "viem";
 import { formatUnits, parseUnits } from "viem";
@@ -14,11 +15,21 @@ import { useAccount, useNetwork, useToken } from "wagmi";
 
 export type AssertionFormProps = ReturnType<typeof useAssertionForm>;
 
-export function useAssertionForm() {
-  const { address: userAddress, isConnected } = useAccount();
+function useChain() {
   const { chain } = useNetwork();
   const chainId = (chain?.id ?? 1) as ChainId;
+  const chainName = chainsById[chainId];
+  const oracleAddress = oov3AddressesByChainId[chainId];
 
+  return useMemo(
+    () => ({ chainId, chainName, oracleAddress }),
+    [chainId, chainName, oracleAddress]
+  );
+}
+
+function useCurrency({ chainId }: { chainId: ChainId }) {
+  // data for currencyDropdownOptions is async, so we set it when its available (see useEffect below)
+  const [currency, setCurrency] = useState<DropdownItem>();
   const { WETH, DAI, USDC } = currenciesByChain[chainId] ?? {};
   const { data: weth } = useToken({
     address: WETH as Address,
@@ -32,52 +43,72 @@ export function useAssertionForm() {
     address: USDC as Address,
     enabled: !!USDC,
   });
-  const currenciesData = { weth, dai, usdc };
-  const [claim, setClaim] = useState("I assert that...");
-  const [claimError, setClaimError] = useState("");
-  const [bond, setBond] = useState("1");
-  const [bondInputError, setBondInputError] = useState("");
-  const [bondIsTooLow, setBondIsTooLow] = useState(false);
-  const [challengePeriod, setChallengePeriod] = useState<DropdownItem>(
-    challengePeriods[2]
-  );
-  // data for currencies is async, so we set it when its available (see useEffect below)
-  const [currency, setCurrency] = useState<DropdownItem>();
-  const currencies = makeCurrencyDropdownOptions();
-  const currencyDetails =
-    currenciesData[currency?.value as keyof typeof currenciesData];
-  const currencyAddress = currencyDetails?.address;
-  const oracleAddress = oov3AddressesByChainId[chainId];
 
-  const minimumBond = useMinimumBond({ currencyAddress, oracleAddress });
+  const currenciesWithTokenData = useMemo(
+    () => ({ weth, dai, usdc }),
+    [dai, usdc, weth]
+  );
+
+  const currencyDropdownOptions = useMemo(
+    () =>
+      Object.entries(currenciesWithTokenData)
+        .map(([currency, details]) =>
+          !!details
+            ? {
+                value: currency,
+                label: `${details.name} - (${details.symbol})`,
+              }
+            : undefined
+        )
+        .filter(Boolean),
+    [currenciesWithTokenData]
+  );
+
+  const currencyDetails =
+    currenciesWithTokenData[
+      currency?.value as keyof typeof currenciesWithTokenData
+    ];
+  const currencyAddress = currencyDetails?.address;
   const decimals = currencyDetails?.decimals ?? 18;
   const currencySymbol = currencyDetails?.symbol ?? "";
-  const bondBigInt = BigInt(parseUnits(bond as `${number}`, decimals));
-  const bondFormatted = formatUnits(bondBigInt, decimals);
-  const challengePeriodBigInt = BigInt(challengePeriod.value);
-  const chainName = chainsById[chainId];
-  const bondIsTooLowError =
-    bondIsTooLow && minimumBond !== undefined
-      ? `Bond must be at least ${formatUnits(minimumBond, decimals)} 
-  ${currencySymbol} on ${chainName}`
-      : undefined;
-  const errors = [claimError, bondInputError, bondIsTooLowError].filter(
-    Boolean
+
+  useEffect(() => {
+    if (!!currencyDropdownOptions && !currency) {
+      setCurrency(currencyDropdownOptions[0]);
+    }
+  }, [currencyDropdownOptions, currency]);
+
+  // if we change to a new chain and the selected currency doesn't exist on that chain, select the first one that does
+  useEffect(() => {
+    if (currency && !currencyDropdownOptions.includes(currency)) {
+      setCurrency(currencyDropdownOptions[0]);
+    }
+  }, [currency, currencyDropdownOptions]);
+
+  return useMemo(
+    () => ({
+      currencies: currencyDropdownOptions,
+      currency,
+      setCurrency,
+      currencyAddress,
+      currencyDetails,
+      currencySymbol,
+      decimals,
+    }),
+    [
+      currency,
+      currencyAddress,
+      currencyDetails,
+      currencyDropdownOptions,
+      currencySymbol,
+      decimals,
+    ]
   );
+}
 
-  useEffect(() => {
-    if (minimumBond !== undefined && bondBigInt < minimumBond) {
-      setBondIsTooLow(true);
-      return;
-    }
-    setBondIsTooLow(false);
-  }, [bondBigInt, minimumBond]);
-
-  useEffect(() => {
-    if (!!currencies && !currency) {
-      setCurrency(currencies[0]);
-    }
-  }, [currencies, currency]);
+function useClaim() {
+  const [claim, setClaim] = useState("I assert that...");
+  const [claimError, setClaimError] = useState("");
 
   useUpdateEffect(() => {
     if (claim === "") {
@@ -87,55 +118,248 @@ export function useAssertionForm() {
     setClaimError("");
   }, [claim]);
 
-  // only weth is supported on goerli, so switch to it if the user is on goerli
-  useEffect(() => {
-    if (chainId === 5) {
-      setCurrency(currencies[0]);
-    }
-  }, [chainId, currencies]);
+  return useMemo(
+    () => ({ claim, setClaim, claimError }),
+    [claim, claimError, setClaim]
+  );
+}
 
-  function makeCurrencyDropdownOptions() {
-    return Object.entries(currenciesData)
-      .map(([currency, details]) =>
-        !!details
-          ? {
-              value: currency,
-              label: `${details.name} - (${details.symbol})`,
-            }
-          : undefined
-      )
-      .filter(Boolean);
-  }
+function useChallengePeriod() {
+  const [challengePeriod, setChallengePeriod] = useState<DropdownItem>(
+    challengePeriods[2]
+  );
+  const challengePeriodBigInt = BigInt(challengePeriod.value);
 
-  return {
+  return useMemo(
+    () => ({ challengePeriod, setChallengePeriod, challengePeriodBigInt }),
+    [challengePeriod, challengePeriodBigInt]
+  );
+}
+
+function useWalletState({
+  currencyAddress,
+  oracleAddress,
+  chainId,
+}: {
+  currencyAddress: Address | undefined;
+  oracleAddress: Address;
+  chainId: ChainId;
+}) {
+  const { address: userAddress, isConnected } = useAccount();
+  const { balance, allowance } = useBalanceAndAllowance({
+    userAddress,
+    currencyAddress,
+    oracleAddress,
     chainId,
-    chainName,
-    claim,
-    claimError,
+  });
+  const balanceFormatted = truncateDecimalString(balance?.formatted ?? "0");
+
+  return useMemo(
+    () => ({
+      userAddress,
+      isConnected,
+      balance: balance?.value ?? BigInt(0),
+      allowance: allowance ?? BigInt(0),
+      balanceFormatted,
+    }),
+    [userAddress, isConnected, balance, allowance, balanceFormatted]
+  );
+}
+
+function useBond({
+  balance,
+  balanceFormatted,
+  allowance,
+  currencyAddress,
+  currencySymbol,
+  chainName,
+  oracleAddress,
+  decimals,
+  isConnected,
+}: {
+  isConnected: boolean;
+  balance: bigint;
+  balanceFormatted: string;
+  allowance: bigint;
+  currencyAddress: Address | undefined;
+  currencySymbol: string;
+  chainName: ChainName;
+  oracleAddress: Address;
+  decimals: number;
+}) {
+  const [bond, setBond] = useState("1");
+  const [bondInputError, setBondInputError] = useState("");
+  const [bondIsTooLow, setBondIsTooLow] = useState(false);
+  const minimumBond = useMinimumBond({ currencyAddress, oracleAddress });
+  const bondBigInt = BigInt(parseUnits(bond as `${number}`, decimals));
+  const bondFormatted = formatUnits(bondBigInt, decimals);
+  const hasApproved = !!allowance && allowance >= bondBigInt;
+  const insufficientFunds = isConnected && balance <= bondBigInt;
+  const bondIsTooLowError =
+    bondIsTooLow && minimumBond !== undefined
+      ? `Bond must be at least ${formatUnits(minimumBond, decimals)} 
+${currencySymbol} on ${chainName}`
+      : undefined;
+  const insufficientFundsError = insufficientFunds
+    ? `Insufficient funds. You have ${balanceFormatted} ${currencySymbol}`
+    : undefined;
+
+  useEffect(() => {
+    if (minimumBond !== undefined && bondBigInt < minimumBond) {
+      setBondIsTooLow(true);
+      return;
+    }
+    setBondIsTooLow(false);
+  }, [bondBigInt, minimumBond]);
+
+  return useMemo(
+    () => ({
+      bond,
+      bondBigInt,
+      bondFormatted,
+      minimumBond,
+      setBond,
+      bondInputError,
+      setBondInputError,
+      bondIsTooLow,
+      hasApproved,
+      insufficientFunds,
+      bondIsTooLowError,
+      insufficientFundsError,
+    }),
+    [
+      bond,
+      bondBigInt,
+      bondFormatted,
+      bondInputError,
+      bondIsTooLow,
+      bondIsTooLowError,
+      hasApproved,
+      insufficientFunds,
+      insufficientFundsError,
+      minimumBond,
+    ]
+  );
+}
+
+export function useAssertionForm() {
+  const { chainId, chainName, oracleAddress } = useChain();
+  const { claim, setClaim, claimError } = useClaim();
+  const { challengePeriod, setChallengePeriod, challengePeriodBigInt } =
+    useChallengePeriod();
+  const {
     currencies,
-    challengePeriods,
-    challengePeriod,
-    challengePeriodBigInt,
     currency,
-    currencySymbol,
+    setCurrency,
+    currencyAddress,
     currencyDetails,
+    currencySymbol,
     decimals,
-    minimumBond,
+  } = useCurrency({ chainId });
+  const { userAddress, isConnected, balance, allowance, balanceFormatted } =
+    useWalletState({
+      currencyAddress,
+      oracleAddress,
+      chainId,
+    });
+  const {
     bond,
     bondBigInt,
     bondFormatted,
-    bondInputError,
-    bondIsTooLow,
-    bondIsTooLowError,
-    userAddress,
-    isConnected,
-    currencyAddress,
-    oracleAddress,
-    errors,
-    setClaim,
-    setCurrency,
+    minimumBond,
     setBond,
-    setBondError: setBondInputError,
-    setChallengePeriod,
-  };
+    bondInputError,
+    setBondInputError,
+    bondIsTooLow,
+    hasApproved,
+    insufficientFunds,
+    bondIsTooLowError,
+    insufficientFundsError,
+  } = useBond({
+    isConnected,
+    balance,
+    balanceFormatted,
+    allowance,
+    currencyAddress,
+    currencySymbol,
+    chainName,
+    oracleAddress,
+    decimals,
+  });
+  const errors = [
+    claimError,
+    bondInputError,
+    bondIsTooLowError,
+    insufficientFundsError,
+  ].filter(Boolean);
+
+  return useMemo(
+    () => ({
+      userAddress,
+      chainId,
+      chainName,
+      claim,
+      claimError,
+      currencies,
+      challengePeriods,
+      challengePeriod,
+      challengePeriodBigInt,
+      currency,
+      currencySymbol,
+      currencyDetails,
+      decimals,
+      minimumBond,
+      bond,
+      bondBigInt,
+      bondFormatted,
+      bondInputError,
+      bondIsTooLow,
+      bondIsTooLowError,
+      isConnected,
+      currencyAddress,
+      oracleAddress,
+      errors,
+      hasApproved,
+      insufficientFunds,
+      insufficientFundsError,
+      setClaim,
+      setCurrency,
+      setBond,
+      setBondInputError,
+      setChallengePeriod,
+    }),
+    [
+      bond,
+      bondBigInt,
+      bondFormatted,
+      bondInputError,
+      bondIsTooLow,
+      bondIsTooLowError,
+      chainId,
+      chainName,
+      challengePeriod,
+      challengePeriodBigInt,
+      claim,
+      claimError,
+      currencies,
+      currency,
+      currencyAddress,
+      currencyDetails,
+      currencySymbol,
+      decimals,
+      errors,
+      hasApproved,
+      insufficientFunds,
+      insufficientFundsError,
+      isConnected,
+      minimumBond,
+      oracleAddress,
+      setBond,
+      setBondInputError,
+      setChallengePeriod,
+      setClaim,
+      setCurrency,
+      userAddress,
+    ]
+  );
 }
