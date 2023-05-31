3d007e6d25c7d045a41818529bc5056109aa6986
ryanwolhuter <dev@ryanwolhuter.com>
2023-05-31 14:47:44 +0200

    add decimals arg to truncate decimal string
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/helpers/numbers.ts
+++ src/helpers/numbers.ts
@@ -1,9 +1,9 @@
-export function truncateDecimalString(s: string) {
+export function truncateDecimalString(s: string, decimals = 3) {
   const hasDecimal = s.includes(".");
   if (hasDecimal) {
     const [wholePart, decimalPart] = s.split(".");
     if (decimalPart.length > 3) {
-      return `${wholePart}.${decimalPart.slice(0, 3)}`;
+      return `${wholePart}.${decimalPart.slice(0, decimals)}`;
     }
   }
   return s;
